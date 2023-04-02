cae687b79b8dbc2e80f5bf5c0ea2494e3054ec6d
nwilliams <nwilliams@pcienergysolutions.com>
2023-04-01 20:40:07 -0500

    removed code with errors in python controller

--- Hackathon Project2/Controllers/PythonController.cs
+++ Hackathon Project2/Controllers/PythonController.cs
@@ -1,5 +1,4 @@
 using System;
-//using Python.Runtime;
 
 namespace CallPythonFromCSharp
 {
@@ -8,19 +7,7 @@ namespace CallPythonFromCSharp
         static void Main(string[] args)
         {
             string pythonScriptPath = "path/to/OpenAI.py";
-
-            /*
-            using (Py.GIL()) // Acquire the Python GIL (Global Interpreter Lock)
-            {
-                dynamic py = Py.Import("__main__");
-                py.exec(File.ReadAllText(pythonScriptPath));
-
-               string test = "this is a test. Respond accordingly";
-
-                dynamic result = py.CustomChatGPT(test);
-                Console.WriteLine($"Result: {result}");
-            }
-            */
+            
         }
     }
 }
